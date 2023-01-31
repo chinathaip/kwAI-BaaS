b3364af21c42ad7060c477f4746fb90f343395ad
chinathaip <cartoonabe@gmail.com>
2023-01-31 19:12:49 +0700

    migrate to oop

--- api/history.php
+++ api/history.php
@@ -2,16 +2,10 @@
 //echo  "yo";
 ini_set('display_errors', 1);
 //phpinfo();
-$conn = mysqli_connect("containers-us-west-187.railway.app", "root", "kuClq9pNEbhqXbFTP2CG", "railway", "6502");
-if ($conn) {
-    echo "Connected to the database!";
-} else {
-    echo "Failed to connect to the database!";
-}
 
-mysqli_set_charset($conn, "utf8");
-$result = mysqli_query($conn, "select * from messages");
-
+$conn = new mysqli("containers-us-west-187.railway.app", "root", "kuClq9pNEbhqXbFTP2CG", "railway", "6502");
+$conn->set_charset("utf8mb4");
+$result = $conn->query("select * from messages");
 $arr = array();
 while ($row = mysqli_fetch_assoc($result)) {
     $arr[] = $row;
